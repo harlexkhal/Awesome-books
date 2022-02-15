068c1f2e508826602f3819ad1e3dfd182f326096
Alex Ibeh <harlexibeh01@gmail.com>
2022-02-15 18:03:00 +0100

    add webpack to project

--- modules/application/application.js
+++ modules/application/application.js
@@ -1,4 +1,4 @@
-import { DateTime } from '../luxon/src/luxon.js';
+import { DateTime } from '../../node_modules/luxon/src/luxon.js';
 
 class Application {
   constructor() {
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import '../assets/index.css';
 import { Application } from '../modules/application/application.js';
 /* eslint-disable */
 const application = new Application();
